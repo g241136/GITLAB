99cec2c120c6e06c8e1020eec5db301dff98bb77
g241136 <gloomy1993@windowslive.com>
2015-04-26 15:45:39 +0700

    First copy of 5 files

--- Program #1.c
+++ Program #1.c
@@ -27,7 +27,7 @@ int chooseNoYes()
 	char sure;
 	do
 		{
-		printf("Are you sured to change this data?(' Y ' or ' N '): ");
+		printf("Are you sured to change this data?(' N ' or ' Y '): ");
 		fgets(input,MAXINPUT,stdin);
 		sscanf(input,"%c",&sure);
 		}
